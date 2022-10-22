3c760f42a96ee86937c65d1a35410d49916dc198
aRandomKiwi <15568941+aRandomKiwi@users.noreply.github.com>
2022-10-22 18:14:58 +0200

    Update assembly version number

--- Source/1.4/Properties/AssemblyInfo.cs
+++ Source/1.4/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2021.0.1.13")]
-[assembly: AssemblyFileVersion("2020.0.1.13")]
+[assembly: AssemblyVersion("2021.0.1.14")]
+[assembly: AssemblyFileVersion("2020.0.1.14")]
