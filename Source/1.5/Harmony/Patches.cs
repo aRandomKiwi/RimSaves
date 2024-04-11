1b4535df4a18e5a8c1024691c2c20ad489bf6380
Kupie <kupokraft@gmail.com>
2024-04-11 18:42:42 -0500

    Add files via upload

---
+++ Source/1.5/Harmony/Patches.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace aRandomKiwi.ARS
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            //HarmonyInstance.Create("rimworld.randomKiwi.FFM").PatchAll(Assembly.GetExecutingAssembly());
+            var harmony = new Harmony("rimworld.randomKiwi.FFM");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
